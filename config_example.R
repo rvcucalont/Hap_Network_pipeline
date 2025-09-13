2280803b24e86639d2c16821f215333d6993bc7f
Roberto Cucalon <rvcucalont@gmail.com>
2025-09-13 10:40:21 -0500

    Modified config_examples.R

--- config_example.R
+++ config_example.R
@@ -2,7 +2,7 @@
 # Copy this file to config.R and edit the paths below for your local setup
 
 # Path to your haplotype table (TSV)
-file_name <- "path/to/your/trimmed_mimic_clade_Cytb_full_aligned_06-18-2025_haplotype.tsv"
+file_name <- "path/to/your/cytb-labels.tsv"
 
 # Path to your metadata Excel file
-Metadata.file <- "path/to/your/MasterSheet_SH_v9.xlsx"
+Metadata.file <- "path/to/your/metadata.xlsx"
