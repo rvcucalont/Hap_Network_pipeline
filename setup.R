517326e3ac6cbc5187e58c7cd90a6596e084bbf2
Roberto Cucalon <rvcucalont@gmail.com>
2025-09-14 09:09:00 -0500

    Created setup file

---
+++ setup.R
@@ -0,0 +1,11 @@
+# This script will be used a source to setup input files paths with user-specific paths
+if(!file.exists("config.yaml")){
+  cat("file_name: ''\nmetadata.file: ''", file = "config.yaml")
+  stop("A config.yaml file has been created. Please edit it with the correct file paths and run the script again.")
+} else {
+  # Load user-specific configuration file with file paths
+  config <- yaml::read_yaml("config.yaml") 
+  cat(paste0("Configuration loaded from config.yaml:\n",
+             "Fasta file: ", config$file_name, "\n",
+             "Metadata file: ", config$metadata.file, "\n"))
+}
