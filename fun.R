6b06c9a558be189672c4f6db6f2037ea6d6be06a
Roberto Cucalon <rvcucalont@gmail.com>
2025-09-13 22:02:07 -0500

    Added fiture to count unMatched Ids. Works on example only not on actual data

--- fun.R
+++ fun.R
@@ -25,8 +25,9 @@ Match.ID <- function(query="Sample1",string="Sample1.A") {
       queryfound <- query
       NoqueryFound <- NA_character_
     } else {
-      NoqueryFound <- query
+      #NoqueryFound <- query
       queryfound <- NA_character_
+      NoqueryFound <- string
     }
     return(list(queryfound,NoqueryFound))
   }
@@ -50,39 +51,53 @@ Get.Matched.ID <- function(queryID,metadata,ByColname,keepCol=NULL) {
   }
   # Initialize an empty vector to store matched IDs
   Matched.ID <- vector()
+  unMatched.ID <- vector()
   # Loop through the tree tip labels and the metadata file to find matches
   # If a match is found, store the matched ID in the Matched.ID vector
   # show progress bar "---" for the for loop
   pb <- txtProgressBar(min = 0, max = length(queryID), style = 3)
   for(i in 1:length(queryID)){
     Matched.ID[i] <- NA
+    unMatched.ID[i] <- NA
     for(j in 1:dim(metadata)[1]){
-      if (!is.na(Match.ID(metadata[[ByColname]][j],queryID[i])[[1]])){
-        Matched <- Match.ID(metadata[[ByColname]][j], queryID[i])
-        Matched.ID[i] <- Matched[[1]]
+      if (!is.na(Match.ID(metadata[[ByColname]][j],queryID[i]))[[1]]){
+        Matched.ID[i] <- Match.ID(metadata[[ByColname]][j], queryID[i])[[1]]
+        #print(paste("matched:",Matched.ID[i],"with",queryID[i]))
         break
-      } 
+      } else {
+        unMatched.ID[i] <- Match.ID(metadata[[ByColname]][j], queryID[i])[[2]]
+        #print(paste("unmatched:",metadata[[ByColname]][j], "with", unMatched.ID[i]))
+        # conditional if the total number of unmatched IDs is equal to the number of rows 
+        #in the metadata then keep the unmatched ID
+        if (length(unMatched.ID) == dim(metadata)[1]) {
+          unMatched.ID[i] <- queryID[i]
+        } else {
+          unMatched.ID[i] <- NA
+        }##
+      }
     }
     setTxtProgressBar(pb, i)
   }
   close(pb)
   Matched.ID <- as.character(na.omit(Matched.ID))
+  #print(Matched.ID)
+  unMatched.ID <- as.character(na.omit(unMatched.ID))
+  #print(unMatched.ID)
   # If keepCol is specified, return a data frame with matched IDs and the specified columns
   if (!is.null(keepCol)) {
     if (!all(keepCol %in% colnames(metadata))) {
       stop("Some columns in keepCol not found in metadata.\n\n")
     }
     Matched.Data <- metadata[match(Matched.ID,metadata[[ByColname]]),c(ByColname,keepCol)]
-    cat("Returning matched IDs along with specified metadata columns.\n\n")
+    cat("Returning metadata subset based on IDs and colnames specified.\n\n")
     Matched.Data
   } else {
     Matched.Data <- metadata[match(Matched.ID,metadata[[ByColname]]),]
-    cat("Returning all metadata columns with match ID. No additional columns specified to keep.\n\n")
+    cat("Returning metadata based on IDs specified.\n\n")
     Matched.Data
   }
   # print the unmatched IDs
-  Unmatched.ID <- metadata[[ByColname]][!metadata[[ByColname]] %in% Matched.ID]
-  cat(paste("Unmatched IDs:", paste(Unmatched.ID, collapse = ", ")))
+  cat(paste("Unmatched IDs:", paste(unMatched.ID, collapse = ", ")))
   
   #Return the matched data
   return(Matched.Data)
