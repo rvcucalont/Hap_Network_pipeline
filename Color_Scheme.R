96f486aebe7c3d15620db68b870aee0709a1b192
Roberto Cucalon <rvcucalont@gmail.com>
2025-09-14 09:03:11 -0500

    Added color scheme on a separate file to select from

---
+++ Color_Scheme.R
@@ -0,0 +1,12 @@
+# color scheme to select from
+# these are color to select from
+tol21rainbow= c("#771155", "#AA4488", "#CC99BB", "#114477", "#4477AA", "#77AADD", 
+                "#117777", "#44AAAA", "#77CCCC", "#117744", "#44AA77", "#88CCAA", 
+                "#777711", "#AAAA44", "#DDDD77", "#774411", "#AA7744", "#DDAA77", 
+                "#771122", "#AA4455", "#DD7788")
+
+# These are the colors selected
+mycol= c("#771155", "#CC99BB", "#114477", "#77AADD", "#117777", 
+                "#44AAAA", "#77CCCC", "#117744", "#44AA77", "#88CCAA", 
+                "#777711", "#AAAA44", "#DDDD77", "#774411", "#AA7744", 
+                "#DDAA77", "#AA4455")
