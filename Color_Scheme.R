69e771d6b064bdc986d822a0e69e1e206aa27b85
Roberto Cucalon <rvcucalont@gmail.com>
2025-09-14 21:46:50 -0500

    Seleceted color schemes for Ecoregions and Ecoregions per Species

--- Color_Scheme.R
+++ Color_Scheme.R
@@ -6,7 +6,12 @@ tol21rainbow= c("#771155", "#AA4488", "#CC99BB", "#114477", "#4477AA", "#77AADD"
                 "#771122", "#AA4455", "#DD7788")
 
 # These are the colors selected
-mycol= c("#771155", "#CC99BB", "#114477", "#77AADD", "#117777", 
-                "#44AAAA", "#77CCCC", "#117744", "#44AA77", "#88CCAA", 
-                "#777711", "#AAAA44", "#DDDD77", "#774411", "#AA7744", 
-                "#DDAA77", "#AA4455")
+Ecoregion = c("#114477", "#77AADD", "#117777", "#77CCCC",
+         "#AAAA44", "#AA7744", "#AA4455", "#DDAA77")
+
+EcoSpecies = c("#114477", "#77AADD", "#77AADD","#117777","#117777","#117777", "#77CCCC",
+              "#AAAA44", "#AAAA44","#AA7744", "#AA4455", "#DDAA77")
+
+
+         
+cat(paste(EcoSpecies, collapse = ","))
