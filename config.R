84f193a41f9e18c2a0333823a639813990f767c8
Roberto Cucalon <rvcucalont@gmail.com>
2025-09-25 17:29:11 -0500

    Updated config.R

--- config.R
+++ config.R
@@ -1,9 +1,21 @@
-# This script will be used a source to setup input files paths with user-specific paths
+# This script will be used as a source to setup input files paths with user-specific paths
 if(!file.exists("config.yaml")){
-  cat("file.name: ''\nmetadata.file: ''", file = "config.yaml")
-  stop("A config.yaml file has been created. Please edit it with the correct file paths and run the script again.")
+  cat("file.name: '' # path/to/fasta\nmetadata.file: '' # path/to/excel.xlxs\n", file = "config.yaml")
+  cat("Please provide fasta file and excel file paths:.\n")
+  Sys.sleep(1)
+  utils::file.edit("config.yaml")
+  stop("A config.yaml file has been created and opened for editing.")
 } else {
   # Load user-specific configuration file with file paths
   config <- yaml::read_yaml("config.yaml") 
-  cat(paste0("Configuration loaded from config.yaml:\n"))
+  cat("Configuration loaded from config.yaml:\n")
+  
+  # Check if required fields are empty
+  if(is.null(config$file.name) || config$file.name == "" ||
+     is.null(config$metadata.file) || config$metadata.file == "") {
+    cat("One or more required fields in config.yaml are empty.\n")
+    cat("Please provide fasta file and excel file paths:.\n")
+    Sys.sleep(1)
+    utils::file.edit("config.yaml")
+  }
 }
