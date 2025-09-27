bf2a26dc1377a7a7355fa818bed0872252811306
Roberto Cucalon <rvcucalont@gmail.com>
2025-09-27 10:46:34 -0500

    Updated script with new function location

--- Get_Matrix_Network.R
+++ Get_Matrix_Network.R
@@ -9,7 +9,7 @@ library(readxl)
 library(ape)
 
 #Load source functions
-source("match2metadata.R")
+source("./dependencies/match2metadata.R")
 #Generate a file called "config.ymal" if it doesn't exist
 #Edit generated file "config.ymal" with your input file names and metadata column names
 source("config.R")
